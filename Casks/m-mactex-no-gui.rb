5b1518617a2d15a0810631b802e4d0dedaaf4899
miccal <miccal.matthews@gmail.com>
2020-04-13 01:49:31 +0800

    Update m-mactex-no-gui.rb

--- Casks/m-mactex-no-gui.rb
+++ Casks/m-mactex-no-gui.rb
@@ -36,5 +36,8 @@ cask 'm-mactex-no-gui' do
                ]
 
   uninstall pkgutil: "org.tug.mactex.texlive#{version.major}",
-            delete:  "/usr/local/texlive/#{version.major}"
+            delete:  [
+                       "/usr/local/texlive/#{version.major}",
+                       "~/Library/texlive/#{version.major}",
+                     ]
 end
