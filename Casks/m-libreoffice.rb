d40b07d0bfca05b1c52a59c0186291f9bbddeb47
miccal <miccal.matthews@gmail.com>
2020-04-16 21:35:06 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -1,9 +1,9 @@
 cask 'm-libreoffice' do
-  version '6.4.3.2'
+  version '6.4.3.0'
   sha256 :no_check
 
-  # url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version.major_minor_patch}_MacOS_x86-64.dmg"
-  url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version.major_minor_patch}_MacOS_x86-64.dmg"
+  # url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   name 'LibreOffice'
   homepage 'https://www.libreoffice.org/'
 
