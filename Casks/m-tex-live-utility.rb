d3a624d807b056f61a305375a663cc6571423340
miccal <miccal.matthews@gmail.com>
2020-04-18 10:06:32 +0800

    Create m-tex-live-utility.rb

---
+++ Casks/m-tex-live-utility.rb
@@ -0,0 +1,10 @@
+cask 'm-tex-live-utility' do
+  version '1.37b1'
+  sha256 :no_check
+
+  url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.dmg"
+  name 'TeX Live Utility'
+  homepage 'https://github.com/amaxwell/tlutility'
+
+  app 'TeX Live Utility.app'
+end
