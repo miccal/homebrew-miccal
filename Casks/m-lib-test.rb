c2f885f6ea93d955f7bacd90afc352f32088ac06
miccal <miccal.matthews@gmail.com>
2020-04-28 20:15:35 +0800

    Create m-lib-test.rb

---
+++ Casks/m-lib-test.rb
@@ -0,0 +1,24 @@
+cask 'm-libreoffice' do
+  version '6.4.3,stable'
+  sha256 :no_check
+
+  if version.after_comma = stable
+    url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version.version.before_comma}/mac/x86_64/LibreOffice_#{version.before_comma}_MacOS_x86-64.dmg"
+  else
+    url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.before_comma}.#{version.after_comma}/mac/x86_64/LibreOffice_#{version.before_comma}.#{version.after_comma}_MacOS_x86-64.dmg"
+  end
+
+  name 'LibreOffice'
+  homepage 'https://www.libreoffice.org/'
+
+  app 'LibreOffice.app'
+  shimscript = "#{staged_path}/soffice.wrapper.sh"
+  binary shimscript, target: 'libreoffice'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      '#{appdir}/LibreOffice.app/Contents/MacOS/soffice' "$@"
+    EOS
+  end
+end
