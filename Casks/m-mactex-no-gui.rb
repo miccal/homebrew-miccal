ff1d4782b7568663f4663d5db46ee67fba18d4a2
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-27 23:51:36 +0800

    Update m-mactex-no-gui.rb

--- Casks/m-mactex-no-gui.rb
+++ Casks/m-mactex-no-gui.rb
@@ -1,39 +1,39 @@
-cask 'm-mactex-no-gui' do
-  version '2020.0407'
+cask "m-mactex-no-gui" do
+  version "2020.0407"
   sha256 :no_check
 
   url "http://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/mactex-#{version.no_dots}.pkg"
-  name 'MacTeX'
-  homepage 'https://www.tug.org/mactex/'
+  name "MacTeX"
+  homepage "https://www.tug.org/mactex/"
 
-  depends_on formula: 'ghostscript'
+  depends_on formula: "ghostscript"
 
   pkg "mactex-#{version.no_dots}.pkg",
       # installer -showChoicesXML -pkg mactex-#{version.no_dots}.pkg -target /
       choices: [
                  {
                    # Ghostscript
-                   'choiceIdentifier' => 'org.tug.mactex.ghostscript9.50',
-                   'choiceAttribute'  => 'selected',
-                   'attributeSetting' => 0,
+                   "choiceIdentifier" => "org.tug.mactex.ghostscript9.50",
+                   "choiceAttribute"  => "selected",
+                   "attributeSetting" => 0,
                  },
                  {
                    # Ghostscript Dynamic Library
-                   'choiceIdentifier' => 'org.tug.mactex.ghostscript9.50libgs',
-                   'choiceAttribute'  => 'selected',
-                   'attributeSetting' => 0,
+                   "choiceIdentifier" => "org.tug.mactex.ghostscript9.50libgs",
+                   "choiceAttribute"  => "selected",
+                   "attributeSetting" => 0,
                  },
                  {
                    # GUI Applications
-                   'choiceIdentifier' => "org.tug.mactex.gui#{version.major}",
-                   'choiceAttribute'  => 'selected',
-                   'attributeSetting' => 0,
+                   "choiceIdentifier" => "org.tug.mactex.gui#{version.major}",
+                   "choiceAttribute"  => "selected",
+                   "attributeSetting" => 0,
                  },
                  {
                    # TeX Live
-                   'choiceIdentifier' => "org.tug.mactex.texlive#{version.major}",
-                   'choiceAttribute'  => 'selected',
-                   'attributeSetting' => 1,
+                   "choiceIdentifier" => "org.tug.mactex.texlive#{version.major}",
+                   "choiceAttribute"  => "selected",
+                   "attributeSetting" => 1,
                  },
                ]
 
