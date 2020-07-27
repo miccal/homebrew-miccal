c56d930269346a3e408c150198d8d096b5bf1086
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-27 23:56:52 +0800

    Update m-mactex-no-gui.rb

--- Casks/m-mactex-no-gui.rb
+++ Casks/m-mactex-no-gui.rb
@@ -11,35 +11,35 @@ cask "m-mactex-no-gui" do
   pkg "mactex-#{version.no_dots}.pkg",
       # installer -showChoicesXML -pkg mactex-#{version.no_dots}.pkg -target /
       choices: [
-                 {
-                   # Ghostscript
-                   "choiceIdentifier" => "org.tug.mactex.ghostscript9.50",
-                   "choiceAttribute"  => "selected",
-                   "attributeSetting" => 0,
-                 },
-                 {
-                   # Ghostscript Dynamic Library
-                   "choiceIdentifier" => "org.tug.mactex.ghostscript9.50libgs",
-                   "choiceAttribute"  => "selected",
-                   "attributeSetting" => 0,
-                 },
-                 {
-                   # GUI Applications
-                   "choiceIdentifier" => "org.tug.mactex.gui#{version.major}",
-                   "choiceAttribute"  => "selected",
-                   "attributeSetting" => 0,
-                 },
-                 {
-                   # TeX Live
-                   "choiceIdentifier" => "org.tug.mactex.texlive#{version.major}",
-                   "choiceAttribute"  => "selected",
-                   "attributeSetting" => 1,
-                 },
-               ]
+        {
+          # Ghostscript
+          "choiceIdentifier" => "org.tug.mactex.ghostscript9.50",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          # Ghostscript Dynamic Library
+          "choiceIdentifier" => "org.tug.mactex.ghostscript9.50libgs",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          # GUI Applications
+          "choiceIdentifier" => "org.tug.mactex.gui#{version.major}",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          # TeX Live
+          "choiceIdentifier" => "org.tug.mactex.texlive#{version.major}",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+      ]
 
   uninstall pkgutil: "org.tug.mactex.texlive#{version.major}",
             delete:  [
-                       "/usr/local/texlive/#{version.major}",
-                       "~/Library/texlive/#{version.major}",
-                     ]
+              "/usr/local/texlive/#{version.major}",
+              "~/Library/texlive/#{version.major}",
+            ]
 end
