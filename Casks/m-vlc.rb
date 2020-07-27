a689a9ee52f0f5eaeaea4fc75b2233eebfdbae0c
Miccal Matthews <miccal.matthews@gmail.com>
2020-07-27 23:52:50 +0800

    Update m-vlc.rb

--- Casks/m-vlc.rb
+++ Casks/m-vlc.rb
@@ -1,19 +1,19 @@
-cask 'm-vlc' do
-  version '3.0.11'
+cask "m-vlc" do
+  version "3.0.11"
   sha256 :no_check
 
   url "https://mirror.aarnet.edu.au/pub/videolan/vlc/#{version}/macosx/vlc-#{version}.dmg"
-  name 'VLC media player'
-  homepage 'https://www.videolan.org/vlc/'
+  name "VLC media player"
+  homepage "https://www.videolan.org/vlc/"
 
-  app 'VLC.app'
+  app "VLC.app"
   shimscript = "#{staged_path}/vlc.wrapper.sh"
-  binary shimscript, target: 'vlc'
+  binary shimscript, target: "vlc"
 
   preflight do
     IO.write shimscript, <<~EOS
       #!/bin/sh
-      '#{appdir}/VLC.app/Contents/MacOS/VLC' "$@"
+      "#{appdir}/VLC.app/Contents/MacOS/VLC" "$@"
     EOS
   end
 end
