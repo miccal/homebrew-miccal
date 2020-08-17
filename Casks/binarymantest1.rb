8c0e50191a0b5aaffa461658e325af1f6dfe6229
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-17 17:12:39 +0800

    Update binarymantest1.rb

--- Casks/binarymantest1.rb
+++ Casks/binarymantest1.rb
@@ -2,12 +2,9 @@ cask "binarymantest1" do
   version "1"
   sha256 :no_check
 
-  url "https://github.com/miccal/personal-files/blob/master/test"
+  url "https://github.com/miccal/personal-files/blob/master/binarytest"
   name "binarymantest1"
   homepage "https://github.com/"
 
-  suite "test"
-
-  binary "#{appdir}/test/binarytest"
-  manpage "#{appdir}/test/mantest.1"
+  binary "#{staged_path}/binarytest"
 end
