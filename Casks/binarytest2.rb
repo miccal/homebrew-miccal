82bbb7a9caac3288b3f20626e3551801077f689f
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-17 17:18:26 +0800

    Update and rename binarymantest2.rb to binarytest2.rb

--- Casks/binarytest2.rb
+++ Casks/binarytest2.rb
@@ -1,9 +1,9 @@
-cask "binarymantest2" do
+cask "binarytest2" do
   version "1"
   sha256 :no_check
 
   url "https://github.com/miccal/personal-files/blob/master/binarytest"
-  name "binarymantest2"
+  name "binarytest2"
   homepage "https://github.com/"
 
   binary "#{staged_path}/binarytest"
