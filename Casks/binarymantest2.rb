eb949a9a21cc3e6877e1255d7e951d8bd6b6089d
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-17 17:16:29 +0800

    Update binarymantest2.rb

--- Casks/binarymantest2.rb
+++ Casks/binarymantest2.rb
@@ -2,10 +2,9 @@ cask "binarymantest2" do
   version "1"
   sha256 :no_check
 
-  url "https://github.com/miccal/personal-files/blob/master/test.zip"
+  url "https://github.com/miccal/personal-files/blob/master/binarytest"
   name "binarymantest2"
   homepage "https://github.com/"
 
   binary "#{staged_path}/binarytest"
-  manpage "#{staged_path}/mantest.1"
 end
