1f9f8b7da2450add8653f101e95dbe852314ceae
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 13:26:15 +0800

    Update m-updatedformula.rb

--- Formula/m-updatedformula.rb
+++ Formula/m-updatedformula.rb
@@ -1,7 +1,7 @@
 class UpdatedFormula < Formula
   desc "m-updatedformula"
   homepage "https://blah.com"
-  url "https://blah.com"
+  url "https://blahblah.com"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
   license "GPL-3.0"
 end
