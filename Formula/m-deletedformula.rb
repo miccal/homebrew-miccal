ace2843cd3456b7daf19e596ae24c318242b575b
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 13:25:54 +0800

    Delete m-deletedformula.rb

--- Formula/m-deletedformula.rb
+++
@@ -1,7 +0,0 @@
-class DeletedFormula < Formula
-  desc "m-deletedformula"
-  homepage "https://blah.com"
-  url "https://blah.com"
-  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
-  license "GPL-3.0"
-end
