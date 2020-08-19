a733b6b7b5df8d717de584744739592a2f1a1e31
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 11:15:13 +0800

    Update update.rb

--- Formula/update.rb
+++ Formula/update.rb
@@ -1,7 +1,7 @@
 class Update < Formula
   desc "Update"
   homepage "https://github.com"
-  url "https://github.com/1"
+  url "https://github.com/update1"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
   license "GPL-3.0"
 end
