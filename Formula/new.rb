a7a2d7efcbb0bf94e5c03b49f5c375a881bc0bf9
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 11:15:49 +0800

    Create new.rb

---
+++ Formula/new.rb
@@ -0,0 +1,7 @@
+class New < Formula
+  desc "Update"
+  homepage "https://github.com"
+  url "https://github.com/new1"
+  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+  license "GPL-3.0"
+end
