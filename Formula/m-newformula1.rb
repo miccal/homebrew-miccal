be1e151942c6764703b58c3c877d20e2ea04510f
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 13:23:47 +0800

    Update m-newformula1.rb

--- Formula/m-newformula1.rb
+++ Formula/m-newformula1.rb
@@ -1,7 +1,7 @@
-class m_newformula1 < Formula
+class newformula1 < Formula
   desc "m-newformula1"
   homepage "https://blah.com"
   url "https://blah.com"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
-  license "blah"
+  license "GPL-3.0"
 end
