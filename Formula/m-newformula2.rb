6754ea939b41c4c42e16f197642cf19b27e8d9c7
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 13:26:51 +0800

    Create m-newformula2.rb

---
+++ Formula/m-newformula2.rb
@@ -0,0 +1,8 @@
+
+class NewFormula2 < Formula
+  desc "m-newformula2"
+  homepage "https://blah.com"
+  url "https://blah.com"
+  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+  license "GPL-3.0"
+end
