62afe4da27d1999448b17250b7bb9d846094f10e
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-19 13:20:19 +0800

    Update and rename new2.rb to m-deletedformula.rb

--- Formula/new2.rb
+++
@@ -1,7 +0,0 @@
-class New2 < Formula
-  desc "New2"
-  homepage "https://github.com"
-  url "https://github.com/new2"
-  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
-  license "GPL-3.0"
-end
