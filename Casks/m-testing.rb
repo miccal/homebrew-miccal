63333f0c03499a0258791a90ae28975d574f5035
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-26 09:41:23 +0800

    Update and rename m-test.rb to m-testing.rb

---
+++ Casks/m-testing.rb
@@ -0,0 +1,11 @@
+cask "m-testing" do
+  version "1"
+  sha256 :no_check
+
+  # test
+  url "https://www.dropbox.com/download?plat=mac&full=1"
+  name "Dropbox"
+  homepage "https://www.dropbox.com/"
+
+  app "Dropbox.app", target: "m-testing.app"
+end
