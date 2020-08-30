ac79d0bc619e03283fd521b9f4827a9b822a2a49
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-30 16:50:41 +0800

    Update m-testing.rb

--- Casks/m-testing.rb
+++ Casks/m-testing.rb
@@ -2,11 +2,11 @@ cask "m-testing" do
   version "1"
   sha256 :no_check
 
-  # test
+  # testing
   url "https://www.dropbox.com/download?plat=mac&full=1"
   name "Dropbox"
   homepage "https://www.dropbox.com/"
 
   app "Dropbox.app", target: "m-testing.app"
-  binary "#{appdir}/Dropbox.app/Contents/MacOS/Dropbox", target: "libreoffice"
+  binary "#{appdir}/Dropbox.app/Contents/MacOS/Dropbox", target: "m-testing"
 end
