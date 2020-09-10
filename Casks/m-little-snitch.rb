e757e7ad6bb9be950c13d8154766bbcd41b28cad
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-11 06:12:58 +0800

    Create m-little-snitch.rb

---
+++ Casks/m-little-snitch.rb
@@ -0,0 +1,18 @@
+cask "m-little-snitch" do
+  version "4.5.2"
+  sha256 :no_check
+
+  url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
+  #url "https://obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version.before_comma}-nightly-(#{version.after_comma}).dmg"
+  name "Little Snitch"
+  homepage "https://www.obdev.at/products/littlesnitch/index.html"
+
+  container type: :naked
+
+  installer manual: "LittleSnitch-#{version}.dmg"
+  #installer manual: "LittleSnitch-#{version.before_comma}-nightly-(#{version.after_comma}).dmg"
+
+  caveats <<~EOS
+    "#{staged_path}/#{token}"
+  EOS
+end
