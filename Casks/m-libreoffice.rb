e2d2aa0eeacae40211cacffe061cd145eae56445
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-30 07:40:33 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -1,5 +1,5 @@
 cask "m-libreoffice" do
-  version "7.1.0.2"
+  version "7.1.0.3"
   sha256 :no_check
 
   #url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version.major_minor_patch}_MacOS_x86-64.dmg"
