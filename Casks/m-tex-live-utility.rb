f785872ea804a6faac8942bc22d6a40d479bd41e
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-11 14:11:09 +0800

    Update m-tex-live-utility.rb

--- Casks/m-tex-live-utility.rb
+++ Casks/m-tex-live-utility.rb
@@ -1,5 +1,5 @@
 cask "m-tex-live-utility" do
-  version "1.42b20"
+  version "1.47"
   sha256 :no_check
 
   url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.dmg"
