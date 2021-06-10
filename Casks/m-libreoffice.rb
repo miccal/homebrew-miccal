83a92960d58a858757c04b4c043f856c5d175f27
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-10 21:28:20 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -1,11 +1,17 @@
 cask "m-libreoffice" do
-  version "7.1.3"
+  version "7.1.4"
   sha256 :no_check
 
   url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version.major_minor_patch}_MacOS_x86-64.dmg"
   #url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   name "LibreOffice"
   homepage "https://www.libreoffice.org/"
+
+  livecheck do
+    url "https://download.documentfoundation.org/libreoffice/testing/"
+    strategy :page_match
+    regex(%r{href="(\d+(?:\.\d+)*)/"}i)
+  end
 
   app "LibreOffice.app"
   shimscript = "#{staged_path}/soffice.wrapper.sh"
