f66f9b1aed8654b3cca5ddbac58324156d04c15b
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-23 22:35:05 +0800

    Update m-little-snitch.rb

--- Casks/m-little-snitch.rb
+++ Casks/m-little-snitch.rb
@@ -1,9 +1,9 @@
 cask "m-little-snitch" do
-  version "5.3.6213"
+  version "5.3"
   sha256 :no_check
 
-  #url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
-  url "https://obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version.before_comma}-nightly-(#{version.after_comma}).dmg"
+  url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
+  #url "https://obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version.before_comma}-nightly-(#{version.after_comma}).dmg"
   name "Little Snitch"
   homepage "https://www.obdev.at/products/littlesnitch/index.html"
 
