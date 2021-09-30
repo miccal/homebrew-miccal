0fe56e03ec80a3c5c24789a1cc5383516be1fcc7
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-01 07:52:02 +0800

    Update m-vlc.rb

--- Casks/m-vlc.rb
+++ Casks/m-vlc.rb
@@ -17,7 +17,7 @@ cask "m-vlc" do
   binary shimscript, target: "vlc"
 
   preflight do
-    IO.write shimscript, <<~EOS
+    File.write shimscript, <<~EOS
       #!/bin/sh
       "#{appdir}/VLC.app/Contents/MacOS/VLC" "$@"
     EOS
