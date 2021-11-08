0811396154097b7996c6d7b0690c7ac966292cff
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-08 09:55:08 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -1,5 +1,5 @@
 cask "m-tor-browser" do
-  version "10.5.10"
+  version "11.0a10"
   sha256 :no_check
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
