37caf01646a33b5e9a6877a9cba9019d656c8a6d
Miccal Matthews <miccal.matthews@gmail.com>
2022-04-28 21:59:13 +0800

    Create m-microsoft-teams-continuous-deployment.rb

---
+++ Casks/m-microsoft-teams-continuous-deployment.rb
@@ -0,0 +1,24 @@
+cask "m-microsoft-teams-continuous-deployment" do
+  version "1.5.00.11767"
+  sha256 :no_check
+
+  url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg",
+      verified: "statics.teams.cdn.office.net/production-osx/"
+  name "Microsoft Teams"
+  desc "Meet, chat, call, and collaborate in just one place"
+  homepage "https://teams.microsoft.com/downloads"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig"
+    regex(/(\d+(?:\.\d+)+).+osx/i)
+  end
+
+  pkg "Teams_osx.pkg"
+
+  uninstall pkgutil:   "com.microsoft.teams",
+            launchctl: "com.microsoft.teams.TeamsUpdaterDaemon",
+            delete:    [
+              "/Library/Logs/Microsoft/Teams",
+              "/Library/Preferences/com.microsoft.teams.plist",
+            ]
+end
