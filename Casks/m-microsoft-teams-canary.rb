e464293ae3d43fa742b49e22b5e4aef828f69ca8
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-10 08:14:46 +0800

    Update m-microsoft-teams-canary.rb

--- Casks/m-microsoft-teams-canary.rb
+++ Casks/m-microsoft-teams-canary.rb
@@ -1,5 +1,5 @@
 cask "m-microsoft-teams-canary" do
-  version "1.5.00.12662"
+  version "1.5.00.12957"
   sha256 :no_check
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/Teams_osx.pkg"
