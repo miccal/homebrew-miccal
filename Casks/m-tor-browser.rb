f2588b188445611bca266758058b4bbc60ed30cb
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-01 04:46:40 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -3,7 +3,7 @@ cask "m-tor-browser" do
   sha256 :no_check
 
   #url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
-  url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx64_ALL.dmg"
+  url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ALL.dmg"
   name "Tor Browser"
   homepage "https://www.torproject.org/"
 
