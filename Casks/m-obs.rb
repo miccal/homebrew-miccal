7d09868455fcb4885819a8be4273d357bbf54f02
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-01 10:08:49 +0800

    Create m-obs.rb

---
+++ Casks/m-obs.rb
@@ -0,0 +1,17 @@
+cask "obs-beta" do
+  version "28.1.0"
+  sha256 :no_check
+
+  url "https://github.com/obsproject/obs-studio/releases/download/#{version}/obs-studio-#{version}-macos-x86_64.dmg"
+  name "OBS"
+  desc "Open-source software for live streaming and screen recording"
+  homepage "https://obsproject.com/forum/list/test-builds.20/"
+
+  livecheck do
+    skip "Check obs and obs-beta"
+  end
+
+  app "OBS.app"
+
+  uninstall delete: "/Library/CoreMediaIO/Plug-Ins/DAL/obs-mac-virtualcam.plugin"
+end
