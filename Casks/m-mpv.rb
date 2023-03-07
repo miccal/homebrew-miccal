695d5162e391c71202ddab970794f5b765ec95ef
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-07 08:43:03 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -2,7 +2,7 @@ cask "m-mpv" do
   version "2023.2"
   sha256 :no_check
 
-  url "https://github.com/vitorgalvao/mpv-dummy/releases/download/#{version}/mpv-dummy.zip"
+  url "https://github.com/vitorgalvao/mpv-dummy/releases/download/#{version}/mpv.DUMMY.dmg"
   name "mpv"
   homepage "https://mpv.io/"
 
