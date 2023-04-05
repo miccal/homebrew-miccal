46025efa1e644890e12b044206ffccee64204fc4
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-05 13:11:42 +0800

    Update m-obs.rb

--- Casks/m-obs.rb
+++ Casks/m-obs.rb
@@ -1,7 +1,7 @@
 cask "m-obs" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "29.0.2"
+  version "29.1.0-beta2"
   sha256 :no_check
 
   url "https://github.com/obsproject/obs-studio/releases/download/#{version}/obs-studio-#{version}-macos-#{arch}.dmg"
