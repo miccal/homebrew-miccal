497d4e9d43050635799ead9c45f594f7a40ce3f5
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-22 10:15:34 +0800

    Create m-microsodft-teams.rb

---
+++ Casks/m-microsodft-teams.rb
@@ -0,0 +1,24 @@
+cask "m-microsoft-teams" do
+  version "23167.1600.2175.1586"
+  sha256 :no_check
+
+  url "https://statics.teams.cdn.office.net/production-osx/#{version}/MicrosoftTeams.pkg"
+  name "Microsoft Teams"
+  homepage "https://teams.microsoft.com/downloads"
+
+  livecheck do
+    url "https://raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig2"
+    regex(%r{Public\s\(R4\)\sbuild.+(\n+.+)+osx/(\d+(?:\.\d+)+)/MicrosoftTeams\.pkg}i)
+  end
+
+  pkg "MicrosoftTeams.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.autoupdate",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  uninstall pkgutil: "com.microsoft.teams2"
+end
