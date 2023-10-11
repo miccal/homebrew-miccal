8f5f5b1e8abcb43281a291226b0811ff7a4797af
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-11 08:13:11 +0800

    Update m-mactex-no-gui.rb

--- Casks/m-mactex-no-gui.rb
+++ Casks/m-mactex-no-gui.rb
@@ -9,7 +9,7 @@ cask "m-mactex-no-gui" do
   livecheck do
     url "http://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/"
     strategy :page_match do |page|
-      match = page.match(/href=.*?mactex-(\d{4})(\d{4})\.pkg/)
+      match = page.match(/href=.*?mactex[._-](\d{4})(\d{4})\.pkg/)
       next if match.blank?
 
       "#{match[1]}.#{match[2]}"
