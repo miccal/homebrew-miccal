9cacbb55acea0f0044dc2687a423d1ccaf186c43
Miccal Matthews <miccal.matthews@gmail.com>
2023-11-03 19:18:33 +0800

    Update and rename dwsim.rb to m-dwsim.rb

--- Casks/m-dwsim.rb
+++ Casks/m-dwsim.rb
@@ -1,12 +1,15 @@
-cask "dwsim" do
+cask "m-dwsim" do
   version "8.6.5"
   sha256 "11a221883aa152721c941b7ea827e2d436da3de7b4e700f857127a7d2d24316d"
 
-  url "https://github.com/DanWBR/dwsim/releases/download/v#{version}/DWSIM.#{version}.dmg",
-      verified: "github.com/DanWBR/dwsim/"
+  url "https://github.com/DanWBR/dwsim/releases/download/v#{version}/DWSIM.#{version}.dmg"
   name "DWSIM"
-  desc "Chemical process simulator"
   homepage "https://dwsim.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "DWSIM.app"
 
