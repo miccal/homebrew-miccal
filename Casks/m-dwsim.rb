378105d1479f22702b052977add377cbcee12c22
Miccal Matthews <miccal.matthews@gmail.com>
2023-11-04 12:47:58 +0800

    Update m-dwsim.rb

--- Casks/m-dwsim.rb
+++ Casks/m-dwsim.rb
@@ -1,6 +1,6 @@
 cask "m-dwsim" do
   version "8.6.5"
-  sha256 "11a221883aa152721c941b7ea827e2d436da3de7b4e700f857127a7d2d24316d"
+  sha256 :no_check
 
   url "https://github.com/DanWBR/dwsim/releases/download/v#{version}/DWSIM.#{version}.dmg"
   name "DWSIM"
