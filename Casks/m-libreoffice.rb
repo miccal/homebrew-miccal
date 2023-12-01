25027ff487059ff6652e8d8195e3bae539277df8
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-01 08:57:50 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -7,7 +7,7 @@ cask "m-libreoffice" do
 
   #url "https://mirror.aarnet.edu.au/pub/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
   #url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
-  url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_#{arch}.dmg
+  url "https://mirror.aarnet.edu.au/pub/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_#{arch}.dmg"
   name "LibreOffice"
   homepage "https://www.libreoffice.org/"
 
