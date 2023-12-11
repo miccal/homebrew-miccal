54f064de75ed030b76c9067b1c772442c9825aba
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-11 21:20:25 +0800

    Create m-obs.rb

---
+++ Casks/m-obs.rb
@@ -0,0 +1,23 @@
+cask "obs-beta" do
+  arch arm: "Apple", intel: "Intel"
+
+  version "30.0.2"
+  sha256 :no_check
+
+  url "https://github.com/obsproject/obs-studio/releases/download/#{version}/OBS-Studio-#{version}-macOS-#{arch}.dmg"
+  name "OBS"
+  homepage "https://obsproject.com/"
+
+  app "OBS.app"
+  shimscript = "#{staged_path}/obs.wrapper.sh"
+  binary shimscript, target: "obs"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/bash
+      exec '#{appdir}/OBS.app/Contents/MacOS/OBS' "$@"
+    EOS
+  end
+
+  uninstall delete: "/Library/CoreMediaIO/Plug-Ins/DAL/obs-mac-virtualcam.plugin"
+end
