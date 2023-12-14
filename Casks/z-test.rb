36cc65bcf4319cbde0c8156b5947f0570aa51300
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-14 09:36:22 +0800

    Create z-test.rb

---
+++ Casks/z-test.rb
@@ -0,0 +1,10 @@
+cask "z-test" do
+  version :latest
+  sha256 :no_check
+
+  url "https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/capcut_capcutpc_0_1.2.4_installer.dmg"
+  name "Test"
+  homepage "http://www.paperstreetsoap.company/"
+
+  app "Test.app"
+end
