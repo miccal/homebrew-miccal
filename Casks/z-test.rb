edcb33b98bcab8a40476e9ed07cceec9cee4366d
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-19 05:46:50 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,8 +1,8 @@
 cask "z-test" do
-  version "1"
+  version :latest
   sha256 :no_check
 
-  url "https://github.com/Blockstream/green_qt/releases/download/release_#{version}/BlockstreamGreen_MacOS_x86_64.zip"
+  url ""
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
