ed8fa02859f77b6be7ecf8313d30d95c2bd829a8
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-20 14:30:44 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -2,7 +2,7 @@ cask "z-test" do
   version :latest
   sha256 :no_check
 
-  url ""
+  url "https://maven.egovframe.go.kr/publist/HDD1/public/eGovFrameDev-4.1.0-Mac-AArch64.dmg"
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
