b3492893eb0376f2e0eb6bd5c2ae8478da7446a5
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-21 16:37:47 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -2,9 +2,14 @@ cask "z-test" do
   version :latest
   sha256 :no_check
 
-  url ""
+  url "http://xactupdate.scottcbrown.org/xACT.xml"
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
+
+  livecheck do
+    url "http://xactupdate.scottcbrown.org/xACT.xml"
+    strategy :sparkle, &:short_version
+  end
 
   app "Test.app"
 end
