b88d996d4e9db2560b1875a7a9e79fe573f12418
Miccal Matthews <miccal.matthews@gmail.com>
2023-12-24 05:24:07 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,10 +1,10 @@
 cask "z-test" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "", intel: ""
 
-  version "1.4.2"
+  version ""
   sha256 :no_check
 
-  url "https://github.com/heyman/heynote/releases/download/v#{version}/Heynote_#{version}_#{arch}.dmg"
+  url ""
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
