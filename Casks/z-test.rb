39080f634c621ea2fcf053fb750fba47278fd9e5
Miccal Matthews <miccal.matthews@gmail.com>
2024-01-05 07:05:21 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,8 +1,8 @@
 cask "z-test" do
-  version "5.0.0"
+  version ""
   sha256 :no_check
 
-  url "https://github.com/wakatime/macos-wakatime/releases/download/v#{version}/macos-wakatime.zip"
+  url ""
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
