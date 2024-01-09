4c831dd5d7d95472e908bbd5ee464aef51f8c998
Miccal Matthews <miccal.matthews@gmail.com>
2024-01-09 11:30:18 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -2,15 +2,15 @@ cask "m-libreoffice" do
   arch arm: "aarch64", intel: "x86-64"
   folder = on_arch_conditional arm: "aarch64", intel: "x86_64"
 
-  version "24.2.0.0.beta1"
+  version "24.2.0.1"
   sha256 :no_check
 
   # Stable:
   #url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
   # RC:
-  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
+  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
   # Dev:
-  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_#{arch}.dmg"
+  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_#{arch}.dmg"
   name "LibreOffice"
   homepage "https://www.libreoffice.org/"
 
@@ -20,9 +20,9 @@ cask "m-libreoffice" do
   end
 
   # Stable, RC:
-  #app "LibreOffice.app"
+  app "LibreOffice.app"
   # Dev:
-  app "LibreOfficeDev.app", target: "LibreOffice.app"
+  #app "LibreOfficeDev.app", target: "LibreOffice.app"
   shimscript = "#{staged_path}/soffice.wrapper.sh"
   binary shimscript, target: "libreoffice"
 
