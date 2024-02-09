caef42de5cf771d3c4178d4af939105545692365
Miccal Matthews <miccal.matthews@gmail.com>
2024-02-09 15:27:47 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,8 +1,8 @@
 cask "z-test" do
-  version "1.1.1"
+  version ""
   sha256 :no_check
 
-  url "https://github.com/kevinhermawan/Ollamac/releases/download/v1.1.1/Ollamac-1.1.1.dmg"
+  url ""
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
