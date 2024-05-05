8007ed36aa3f87f86debf606ee054ad46a330ebe
Miccal Matthews <miccal.matthews@gmail.com>
2024-05-05 11:41:34 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,8 +1,8 @@
 cask "z-test" do
-  version ""
+  version "1.9.2"
   sha256 :no_check
 
-  url ""
+  url "https://github.com/Chilledheart/yass/releases/download/#{version}/yass-macos-release-arm64-#{version}.dmg"
   name "Test"
   homepage "http://www.paperstreetsoap.company/"
 
