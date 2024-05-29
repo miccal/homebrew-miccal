c7b26ded7d27d57780afaa9516bd7945b64763d6
Miccal Matthews <miccal.matthews@gmail.com>
2024-05-29 14:21:08 +0800

    Create m-microsoft-defender.rb

---
+++ Casks/m-microsoft-defender.rb
@@ -0,0 +1,32 @@
+cask "m-microsoft-defender" do
+  version "101.24032.0006"
+  sha256 :no_check
+
+  url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Defender_#{version}_Individuals_Installer.pkg"
+  name "Microsoft Defender"
+  homepage "https://www.microsoft.com/security/"
+
+  livecheck do
+    url "https://aka.ms/MacDefender"
+    strategy :extract_plist
+  end
+
+  pkg "Microsoft_Defender_#{version}_Individuals_Installer.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.package.Microsoft_AutoUpdate.app",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  uninstall script:  {
+              executable: "/Library/Application Support/Microsoft/Defender/uninstall/uninstall",
+              sudo:       true,
+            },
+            pkgutil: [
+              "com.microsoft.dlp.agent",
+              "com.microsoft.dlp.daemon",
+              "com.microsoft.dlp.ux",
+            ]
+end
