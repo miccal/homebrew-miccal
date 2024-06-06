04f6173070d7d5b55a52f9a3ad04ce0a53c8564a
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-06 12:16:43 +0800

    Update z-test.rb

--- Casks/z-test.rb
+++ Casks/z-test.rb
@@ -1,15 +1,34 @@
 cask "z-test" do
-  version ""
-  sha256 :no_check
+  version "2.6.1,2.6.0"
+  sha256 "c1a1ff2263b3ded7816c8d2e7309e77f3d0901d488c8559281cfd4d2bb7b891b"
 
-  url ""
-  name "Test"
-  homepage "http://www.paperstreetsoap.company/"
+  url "https://github.com/kingToolbox/WindTerm/releases/download/#{version.csv.second}/WindTerm_#{version.csv.first}_Mac_Portable_x86_64.dmg"
+  name "WindTerm"
+  desc "SSH/SFTP/Shell/Telnet/Serial terminal"
+  homepage "https://github.com/kingToolbox/WindTerm"
 
- livecheck do
+  livecheck do
     url :url
-    strategy :github_latest
+    regex(/^(\d+(?:\.\d+)+)/i)
+    strategy :github_latest do |item, regex|
+      version = item["tag_name"][regex, 1]
+      next if version.blank?
+
+      "#{version},#{item["tag_name"]}"
+    end
   end
 
-  app "Test.app"
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "WindTerm.app"
+
+  zap trash: [
+    "~/Library/Preferences/KingToolbox.WindTerm.plist",
+    "~/Library/Saved Application State/KingToolbox.WindTerm.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
 end
