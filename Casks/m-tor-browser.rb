b33d94028f5fe9a2b526ab6312478d620b7af7c1
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-13 21:57:28 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -2,7 +2,7 @@ cask "m-tor-browser" do
   version "13.0.16"
   sha256 :no_check
 
-  url "https://archive.torproject.org/tor-package-archive/torbrowser/#{version}/tor-browser-macos-#{version}.dmg"
+  url "https://archive.torproject.org/tor-package-archive/torbrowser/#{version.scv.second}/tor-browser-macos-#{version.scv.second}.dmg"
   name "Tor Browser"
   homepage "https://www.torproject.org/"
 
@@ -12,7 +12,7 @@ cask "m-tor-browser" do
       match = page.match(/(\d+(?:.\d+)+).*(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2})/i)
       next if match.blank?
 
-      "#{match[2].tr("-",".")},#{match[3].tr(":",".")},#{match[1]}"
+      "#{match[2].tr("-",".")}.#{match[3].tr(":",".")},#{match[1]}"
     end
   end
 
