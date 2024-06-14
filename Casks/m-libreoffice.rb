47f7000b0943e85d68ea6b8ccefe3498a6c6b967
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-14 09:43:57 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -16,24 +16,7 @@ cask "m-libreoffice" do
 
   livecheck do
     url "https://download.documentfoundation.org/libreoffice/testing/"
-    regex(/(.*)[._-]MacOS[._-]#{arch}\.dmg/i)
-    strategy :page_match do |page, regex|
-      # Match years from directories
-      years = page.scan(%r{href=["']v?(\d+(?:\.\d+)+)/?["' >]}i)
-                  .flatten
-                  .uniq
-                  .map { |v| Version.new(v) }
-                  .sort
-      next if years.blank?
-
-      # Fetch the page for the newest year directory
-      newest_year = years.last.to_s
-      year_page = Homebrew::Livecheck::Strategy.page_content(URI.join(@url, newest_year, "/mac/#{folder}/").to_s)
-      next if year_page[:content].blank?
-
-      # Match version from source tarball filenames
-      year_page[:content].scan(regex).flatten
-    end
+    regex(%r{href=["']v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
   # Stable, RC:
