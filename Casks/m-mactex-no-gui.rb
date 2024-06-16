0890eb054a7fea2e6e8612d2c317ada82db67e02
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-16 21:24:45 +0800

    Update m-mactex-no-gui.rb

--- Casks/m-mactex-no-gui.rb
+++ Casks/m-mactex-no-gui.rb
@@ -2,10 +2,8 @@ cask "m-mactex-no-gui" do
   version "2024.0312"
   sha256 :no_check
 
-  url "http://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/mactex-#{version.no_dots}.pkg",
-      verified: "mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/"
+  url "http://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/mactex-#{version.no_dots}.pkg"
   name "MacTeX"
-  desc "Full TeX Live distribution without GUI applications"
   homepage "https://www.tug.org/mactex/"
 
   livecheck do
@@ -13,7 +11,6 @@ cask "m-mactex-no-gui" do
   end
 
   depends_on formula: "ghostscript"
-  depends_on macos: ">= :mojave"
 
   pkg "mactex-#{version.no_dots}.pkg",
       # installer -showChoicesXML -pkg filename.pkg -target /
