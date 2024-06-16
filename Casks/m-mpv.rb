2745545ccebf4663c3929c5196a97d1ccc8530a6
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-16 21:26:06 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -2,10 +2,8 @@ cask "m-mpv" do
   version "2023.2"
   sha256 :no_check
 
-  url "https://github.com/vitorgalvao/mpv-dummy/releases/download/#{version}/mpv.DUMMY.dmg",
-      verified: "github.com/vitorgalvao/mpv-dummy/"
+  url "https://github.com/vitorgalvao/mpv-dummy/releases/download/#{version}/mpv.DUMMY.dmg"
   name "mpv"
-  desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io/"
 
   livecheck do
