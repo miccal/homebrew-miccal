47abf76c5eb8d4de62cb3504b1d1a6b96b074ee2
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:56:08 +0800

    Create m-texshop.rb

---
+++ Casks/m-texshop.rb
@@ -0,0 +1,14 @@
+cask "texshop" do
+  version "5.36"
+  sha256 :no_check
+
+  url "https://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
+  name "TeXShop"
+  homepage "https://pages.uoregon.edu/koch/texshop/"
+
+  livecheck do
+    cask "texshop"
+  end
+
+  app "TeXShop.app"
+end
