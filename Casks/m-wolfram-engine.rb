875a114b11b06cbde3f3ff240b37032965413e51
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:59:58 +0800

    Create m-wolfram-engine.rb

---
+++ Casks/m-wolfram-engine.rb
@@ -0,0 +1,15 @@
+cask "m-wolfram-engine" do
+  version "14.0.0.0"
+  sha256 :no_check
+
+  url "https://files.wolframcdn.com/packages/Homebrew/#{version}/WolframEngine_#{version.major_minor_patch}_MAC.dmg"
+  name "Wolfram Engine"
+  homepage "https://www.wolfram.com/engine/"
+
+  livecheck do
+    cask "wolfram-engine"
+  end
+
+  app "Wolfram Engine.app"
+  binary "#{appdir}/Wolfram Engine.app/Contents/Resources/Wolfram Player.app/Contents/MacOS/wolframscript"
+end
