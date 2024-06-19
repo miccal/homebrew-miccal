f7eaba91fc790860c8e78a742798d197d6746118
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:02:16 +0800

    Create m-apparency.rb

---
+++ Casks/m-apparency.rb
@@ -0,0 +1,23 @@
+cask "apparency" do
+  version "2.0,426"
+  sha256 :no_check
+
+  url "https://mothersruin.com/software/downloads/Apparency.dmg"
+
+  livecheck do
+    url "https://www.mothersruin.com/software/Apparency/data/ApparencyVersionInfo.plist"
+    strategy :xml do |xml|
+      short_version = xml.elements["//key[text()='CFBundleShortVersionString']"]&.next_element&.text&.strip
+      version = xml.elements["//key[text()='CFBundleVersion']"]&.next_element&.text&.strip
+      next if short_version.blank? || version.blank?
+
+      "#{short_version},#{version}"
+    end
+  end
+
+  name "Apparency"
+  homepage "https://www.mothersruin.com/software/Apparency/"
+
+  app "Apparency.app"
+  binary "#{appdir}/Apparency.app/Contents/MacOS/appy"
+end
