a96296b4a96ee85d5ca67ef737b184efd8399776
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:37:50 +0800

    Update m-ipe.rb

--- Casks/m-ipe.rb
+++ Casks/m-ipe.rb
@@ -2,8 +2,7 @@ cask "m-ipe" do
   arch arm: "arm", intel: "intel"
 
   version "7.2.29"
-  sha256 arm:   "25afb5d6bd0f4f51e27823d86de470d6caa7600e8ee5111f633fa10879904b8c",
-         intel: "8bfb970614be423f1bf716217be3db6faa4676c5fa55a51ef81a2e3c6d6edc4d"
+  sha256 :no_check
 
   url "https://github.com/otfried/ipe/releases/download/v#{version}/ipe-#{version}-mac-#{arch}.dmg"
   name "Ipe"
