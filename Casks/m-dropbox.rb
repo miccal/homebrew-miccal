5b2fb9b1ea7b727556a9eab737a6e3774a6813e3
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:14:07 +0800

    Create m-dropbox.rb

---
+++ Casks/m-dropbox.rb
@@ -0,0 +1,18 @@
+cask "m-dropbox" do
+  arch arm: "&arch=arm64"
+
+  version "202.3.5441"
+  sha256 arm:   "6a9364770ac96510a75741242ac9bdb3f0c289727407eaa07fd2bc2a592ca831",
+         intel: "10ed113d1a4c7218d61bf34b3f5ebf1aede711ce328547a4587328b0b3f74aa2"
+
+  url "https://www.dropbox.com/download?build=#{version}&plat=mac&rtoken=&type=full#{arch}"
+  name "Dropbox"
+  homepage "https://www.dropboxforum.com/t5/Dropbox-desktop-client-builds/bd-p/101003016"
+
+  livecheck do
+    url :homepage
+    regex(/Beta\sBuild\s(\d+(?:\.\d+)+)/i)
+  end
+
+  app "Dropbox.app"
+end
