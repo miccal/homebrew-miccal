26430fdc1b00c3af1c390021dbeab1b8c74bd30f
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-19 17:55:03 +0800

    Create m-suspicious-package.rb

---
+++ Casks/m-suspicious-package.rb
@@ -0,0 +1,15 @@
+cask "m-suspicious-package" do
+  version "4.4,1088"
+  sha256 :no_check
+
+  url "https://www.mothersruin.com/software/downloads/SuspiciousPackage.dmg"
+  name "Suspicious Package"
+  homepage "https://www.mothersruin.com/software/SuspiciousPackage/"
+
+  livecheck do
+    cask "suspicious-package"
+  end
+  
+  app "Suspicious Package.app"
+  binary "#{appdir}/Suspicious Package.app/Contents/SharedSupport/spkg"
+end
