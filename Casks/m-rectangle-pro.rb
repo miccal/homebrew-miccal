991e58c87e1c13eb2644d28970ca886a2b44a19a
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-20 14:44:28 +0800

    Update m-rectangle-pro.rb

--- Casks/m-rectangle-pro.rb
+++ Casks/m-rectangle-pro.rb
@@ -3,8 +3,6 @@ cask "m-rectangle-pro" do
   sha256 :no_check
 
   url "https://rectangleapp.com/pro/downloads/Rectangle%20Pro%20#{version}.dmg"
-  name "Rectangle Pro"
-  homepage "https://rectangleapp.com/pro"
 
   livecheck do
     cask "rectangle-pro"
