7cb76482a66135f61dc85a4dafb6e5283d8e1344
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-20 14:37:28 +0800

    Update m-folx.rb

--- Casks/m-folx.rb
+++ Casks/m-folx.rb
@@ -3,8 +3,6 @@ cask "m-folx" do
   sha256 :no_check
 
   url "https://cdn.electronic.us/products/folx/mac/download/downloader_mac.dmg"
-  name "Folx"
-  homepage "https://mac.eltima.com/download-manager.html"
 
   livecheck do
     cask "folx"
