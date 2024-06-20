078ab0adfb7933c8b38e43e4676fb93218487e86
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-20 14:40:48 +0800

    Update m-microsoft-outlook.rb

--- Casks/m-microsoft-outlook.rb
+++ Casks/m-microsoft-outlook.rb
@@ -3,8 +3,6 @@ cask "m-microsoft-outlook" do
   sha256 :no_check
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
-  name "Microsoft Outlook"
-  homepage "https://www.microsoft.com/en-us/microsoft-365/outlook/outlook-for-business"
 
   livecheck do
     cask "microsoft-outlook"
