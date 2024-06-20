e4577bf4d1e510f79b5e99323136c5af087b41b5
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-20 14:41:06 +0800

    Update m-microsoft-teams.rb

--- Casks/m-microsoft-teams.rb
+++ Casks/m-microsoft-teams.rb
@@ -3,8 +3,6 @@ cask "m-microsoft-teams" do
   sha256 :no_check
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/MicrosoftTeams.pkg"
-  name "Microsoft Teams"
-  homepage "https://www.microsoft.com/en-us/microsoft-teams/group-chat-software"
 
   livecheck do
     cask "microsoft-teams"
