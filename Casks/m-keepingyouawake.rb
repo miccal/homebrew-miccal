c507eb26c77380f9c16264d9bd12c10fbf67cd6e
Miccal Matthews <miccal.matthews@gmail.com>
2024-06-21 09:35:21 +0800

    Update m-keepingyouawake.rb

--- Casks/m-keepingyouawake.rb
+++ Casks/m-keepingyouawake.rb
@@ -5,7 +5,14 @@ cask "m-keepingyouawake" do
   url "https://github.com/newmarcel/KeepingYouAwake/releases/download/#{version}/KeepingYouAwake-#{version}.zip"
 
   livecheck do
-    cask "keepingyouawake"
+    url :url
+    strategy :github_releases do |json|
+      json.map do |release|
+        next if release["draft"]
+
+        release["tag_name"]
+      end
+    end
   end
 
   app "KeepingYouAwake.app"
