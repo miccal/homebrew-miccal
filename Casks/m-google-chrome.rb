474b741a924f9a6b48a8dba1722481ed8fbb789c
Miccal Matthews <miccal.matthews@gmail.com>
2024-07-11 03:36:51 +0800

    Update m-google-chrome.rb

--- Casks/m-google-chrome.rb
+++ Casks/m-google-chrome.rb
@@ -6,8 +6,8 @@ cask "m-google-chrome" do
 
   livecheck do
     url "https://chromiumdash.appspot.com/fetch_releases?channel=Extended&platform=Mac"
-    strategy :page_match do |page|
-      JSON.parse(page)[0]["version"]
+    strategy :json do |json|
+      json.map { |item| item["version"] }
     end
   end
 
