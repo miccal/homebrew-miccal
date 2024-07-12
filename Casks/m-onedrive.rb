ace71c83aa77c46b5e3ae678d94fde65b9bb469b
Miccal Matthews <miccal.matthews@gmail.com>
2024-07-13 06:58:11 +0800

    Update m-onedrive.rb

--- Casks/m-onedrive.rb
+++ Casks/m-onedrive.rb
@@ -1,5 +1,5 @@
 cask "m-onedrive" do
-  version "24.108.0528.0005"
+  version "24.116.0609.0005"
   sha256 :no_check
 
   url "https://oneclient.sfx.ms/Mac/Installers/#{version}/universal/OneDrive.pkg"
