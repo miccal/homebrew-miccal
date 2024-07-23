7a551c840f28c7cf8b9b0723890f55e0eec8c5bf
Miccal Matthews <miccal.matthews@gmail.com>
2024-07-23 08:23:59 +0800

    Update m-obs.rb

--- Casks/m-obs.rb
+++ Casks/m-obs.rb
@@ -1,7 +1,7 @@
 cask "m-obs" do
   arch arm: "Apple", intel: "Intel"
 
-  version "30.2.0"
+  version "30.2.1"
   sha256 :no_check
 
   url "https://github.com/obsproject/obs-studio/releases/download/#{version}/OBS-Studio-#{version}-macOS-#{arch}.dmg"
