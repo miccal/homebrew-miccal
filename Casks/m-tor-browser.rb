0f79ee2a402fd9329ff9f96c842032cb40b682b7
Miccal Matthews <miccal.matthews@gmail.com>
2024-07-23 09:31:48 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -2,7 +2,7 @@ cask "m-tor-browser" do
   version "13.5.1"
   sha256 :no_check
 
-  url "https://dist.torproject.org/torbrowser/#{version.csv.second}/tor-browser-macos-#{version.csv.second}.dmg"
+  url "https://dist.torproject.org/torbrowser/#{version}/tor-browser-macos-#{version}.dmg"
 
   livecheck do
     url "https://dist.torproject.org/torbrowser/?C=M;O=D"
