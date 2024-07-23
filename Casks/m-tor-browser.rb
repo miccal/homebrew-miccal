36c198a4985fa4fe9825e296956125d08bd46272
Miccal Matthews <miccal.matthews@gmail.com>
2024-07-23 09:29:14 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -1,5 +1,5 @@
 cask "m-tor-browser" do
-  version "2024.07.10.12.30,13.5.1"
+  version "13.5.1"
   sha256 :no_check
 
   url "https://dist.torproject.org/torbrowser/#{version.csv.second}/tor-browser-macos-#{version.csv.second}.dmg"
@@ -7,12 +7,6 @@ cask "m-tor-browser" do
   livecheck do
     url "https://dist.torproject.org/torbrowser/?C=M;O=D"
     regex(%r{href=["']?v?(\d+(?:.\d+)+)/?["' >]}i)
-    strategy :page_match do |page, regex|
-      match = page.match(regex)
-      next if match.blank?
-
-      match[1]
-    end
   end
 
   # Stable:
