bb96fd7830d361867f329496bc234eb6b1f82fc3
Miccal Matthews <miccal.matthews@gmail.com>
2024-08-07 10:22:14 +0800

    Update m-devtoys.rb

--- Casks/m-devtoys.rb
+++ Casks/m-devtoys.rb
@@ -1,7 +1,7 @@
 cask "m-devtoys" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.4.0"
+  version "2.0.5.0"
   sha256 :no_check
 
   url "https://github.com/DevToys-app/DevToys/releases/download/v#{version}/devtoys_osx_#{arch}.zip"
