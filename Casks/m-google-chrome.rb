27d1391964f36a236c6bde9904cdf1a0002c3576
Miccal Matthews <miccal.matthews@gmail.com>
2024-08-22 05:24:05 +0800

    Update m-google-chrome.rb

--- Casks/m-google-chrome.rb
+++ Casks/m-google-chrome.rb
@@ -1,5 +1,5 @@
 cask "m-google-chrome" do
-  version "128.0.6613.36"
+  version "128.0.6613.85"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
