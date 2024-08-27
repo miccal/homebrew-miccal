008319029484ceda2636fe703c5e6b2297e0b8f5
Miccal Matthews <miccal.matthews@gmail.com>
2024-08-27 20:02:09 +0800

    Update m-wolfram-engine.rb

--- Casks/m-wolfram-engine.rb
+++ Casks/m-wolfram-engine.rb
@@ -21,7 +21,7 @@ cask "m-wolfram-engine" do
     #{token} can be added as a JupyterLab kernel by running the command
       git clone https://github.com/WolframResearch/WolframLanguageForJupyter.git
     in a suitable directory, then running the command
-      cd WolframLanguageForJupyter ; wolframscript -c configure-jupyter.wls add
+      cd WolframLanguageForJupyter ; wolframscript -code configure-jupyter.wls add
     The postflight block removes the JupyterLab kernel for the previous version of #{token}.
     To check the current list of JupyterLab kernels, run the command
       jupyter kernelspec list
