6ebe333ed8de8eb68ec501628f239dba6fdf781e
Miccal Matthews <miccal.matthews@gmail.com>
2024-08-27 13:13:12 +0800

    Update m-wolfram-engine.rb

--- Casks/m-wolfram-engine.rb
+++ Casks/m-wolfram-engine.rb
@@ -19,7 +19,7 @@ cask "m-wolfram-engine" do
 
   caveats <<~EOS
     The postflight block removes the JupyterLab kernel for the previous version of #{token}.
-    To check current list of JupyterLab kernels, run the command
+    To check the current list of JupyterLab kernels, run the command
       jupyter kernelspec list
   EOS
 end
