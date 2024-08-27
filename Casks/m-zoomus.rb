76f5557ce460eda51af0b3b2858261eab1bb4997
Miccal Matthews <miccal.matthews@gmail.com>
2024-08-27 13:01:25 +0800

    Update m-zoomus.rb

--- Casks/m-zoomus.rb
+++ Casks/m-zoomus.rb
@@ -19,6 +19,6 @@ cask "m-zoomus" do
   end
 
   caveats <<~EOS
-    The postflight block removes the Global Launch Daemon of #{token}, as it casues issues on Arm-based Mac's.
+    The postflight block removes the Global Launch Daemon of #{token}, as it causes issues on Arm-based Mac's.
   EOS
 end
