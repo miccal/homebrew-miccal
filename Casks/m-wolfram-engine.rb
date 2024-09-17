e017177bc71cb9d3c39c87e2129ef71ffa074900
Miccal Matthews <miccal.matthews@gmail.com>
2024-09-17 15:19:07 +0800

    Update m-wolfram-engine.rb

--- Casks/m-wolfram-engine.rb
+++ Casks/m-wolfram-engine.rb
@@ -13,7 +13,7 @@ cask "m-wolfram-engine" do
 
   postflight do
     system_command "/bin/rm",
-                   args: ["-r", "$HOME/Library/Jupyter/kernels/wolframlanguage14.1"],
+                   args: ["-r", "#{Dir.home}/Library/Jupyter/kernels/wolframlanguage14.1"],
                    sudo: true
   end
 
