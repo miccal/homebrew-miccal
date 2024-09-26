f17961a1d5aac39468e22b799b2affb55fe92dcf
Miccal Matthews <miccal.matthews@gmail.com>
2024-09-26 11:42:48 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -1,13 +1,13 @@
 cask "m-mpv" do
   arch arm: "14-arm", intel: "13-intel"
 
-  version "2023.2"
+  version "2024.09.26"
   sha256 :no_check
 
   url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-macos-#{arch}.zip"
 
   livecheck do
-    skip "Version is the date"
+    skip "Version is the date in [year].[month].[day]"
   end
 
   app "mpv.app"
