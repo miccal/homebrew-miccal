8c78437e3212372be3ba4398a3f3cb80f61a555f
Miccal Matthews <miccal.matthews@gmail.com>
2024-09-28 06:24:31 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -1,20 +1,24 @@
 cask "m-mpv" do
   arch arm: "14-arm", intel: "13-intel"
 
-  version "2024.09.28"
+  version "20240926"
   sha256 :no_check
 
   url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-macos-#{arch}.zip"
 
   livecheck do
-    skip
+    url "https://api.github.com/repos/mpv-player/mpv/branches/master"
+    strategy :json do |json|
+      date = json.dig("commit", "commit", "author", "date")
+      Date.parse(date).strftime("%Y%m%d").to_s
+    end
   end
 
   app "mpv.app"
 
   caveats <<~EOS
     #{token} installs a test build of mpv.app directly from github.com/mpv-player/mpv, built using the most recent commit and pull request.
-    The version is simply the date in the format {year}.{month}.{day}, and is updated on a weekly basis.
+    The version is simply the date of the most recent commit and pull request in the format {year}{month}{day}.
     Run the command
       git ls-remote https://github.com/mpv-player/mpv.git HEAD
     to list the reference of the the most recent commit and pull request.
