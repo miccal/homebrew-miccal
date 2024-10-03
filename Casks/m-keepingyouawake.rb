70d47419a1854df007914bbbedd79aade06173a7
Miccal Matthews <miccal.matthews@gmail.com>
2024-10-03 18:12:41 +0800

    Update m-keepingyouawake.rb

--- Casks/m-keepingyouawake.rb
+++ Casks/m-keepingyouawake.rb
@@ -1,5 +1,5 @@
 cask "m-keepingyouawake" do
-  version "1.6.6beta1"
+  version "1.6.6beta2"
   sha256 :no_check
 
   url "https://github.com/newmarcel/KeepingYouAwake/releases/download/#{version}/KeepingYouAwake-#{version}.zip"
