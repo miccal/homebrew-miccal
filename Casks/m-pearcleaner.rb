86d4ed43a65dcbd924d480577ab8e3338feb75db
Miccal Matthews <miccal.matthews@gmail.com>
2024-10-08 18:40:57 +0800

    Create m-pearcleaner.rb

---
+++ Casks/m-pearcleaner.rb
@@ -0,0 +1,19 @@
+cask "pearcleaner" do
+  version "3.9.1"
+  sha256 :no_check
+
+  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip",
+
+  livecheck do
+    url :url
+    strategy :github_releases do |json|
+      json.map do |release|
+        next if release["draft"]
+
+        release["tag_name"]
+      end
+    end
+  end
+
+  app "Pearcleaner.app"
+end
