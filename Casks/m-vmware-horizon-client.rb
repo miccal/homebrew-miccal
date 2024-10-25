e560e343728f3e4bd58112844dd110b98db60686
Miccal Matthews <miccal.matthews@gmail.com>
2024-10-26 00:08:43 +0800

    Update m-vmware-horizon-client.rb

--- Casks/m-vmware-horizon-client.rb
+++ Casks/m-vmware-horizon-client.rb
@@ -1,5 +1,5 @@
 cask "m-vmware-horizon-client" do
-  version "2406-8.13.0-10025792799,CART25FQ2_MAC_2406"
+  version "2406.1-8.13.1-11490701655,CART25FQ2_MAC_2406.1"
   sha256 :no_check
 
   url "https://download3.omnissa.com/software/#{version.csv.second}/VMware-Horizon-Client-#{version.csv.first}.dmg"
