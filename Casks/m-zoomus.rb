696522e712dc7948380bfbf4f44e6d03830d74ec
Miccal Matthews <miccal.matthews@gmail.com>
2024-10-27 21:36:14 +0800

    Update m-zoomus.rb

--- Casks/m-zoomus.rb
+++ Casks/m-zoomus.rb
@@ -1,7 +1,7 @@
 cask "m-zoomus" do
   arch arm: "arm64/"
 
-  version "6.2.5.41699"
+  version "6.2.6.41824"
   sha256 :no_check
 
   url "https://cdn.zoom.us/prod/#{version}/#{arch}zoomusInstallerFull.pkg"
