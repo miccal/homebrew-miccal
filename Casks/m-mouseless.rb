42bae97689c81641bafad88c687783053557b468
Miccal Matthews <miccal.matthews@gmail.com>
2024-12-16 14:00:19 +0800

    Update m-mouseless.rb

--- Casks/m-mouseless.rb
+++ Casks/m-mouseless.rb
@@ -2,7 +2,18 @@ cask "m-mouseless" do
   version "0.2.1"
   sha256 :no_check
 
-  url "https://mouseless.click/mouseless-installer_v#{version}.dmg"
+  url "https://github.com/croian/mouseless-issues/releases/download/v#{version}/mouseless-installer_v#{version}.dmg"
+
+  livecheck do
+    url :url
+    strategy :github_releases do |json|
+      json.map do |release|
+        next if release["draft"]
+
+        release["tag_name"]
+      end
+    end
+  end
 
   app "Mouseless.app"
 end
