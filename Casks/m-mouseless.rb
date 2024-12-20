fb3e5febe30a9d7817f296b34628a29c6c0297db
Miccal Matthews <miccal.matthews@gmail.com>
2024-12-21 03:21:17 +0800

    Update m-mouseless.rb

--- Casks/m-mouseless.rb
+++ Casks/m-mouseless.rb
@@ -2,21 +2,10 @@ cask "m-mouseless" do
   version "0.2.1"
   sha256 :no_check
 
-  url "https://github.com/croian/mouseless-issues/releases/download/v#{version}/mouseless-installer_v#{version}.dmg"
+  url "https://mouseless.click/mouseless-installer_v#{version}.dmg"
 
   livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        next if release["draft"]
-
-        match = release["tag_name"]&.match(regex)
-        next if match.blank?
-
-        match[1]
-      end
-    end
+    cask "mouseless"
   end
 
   app "Mouseless.app"
