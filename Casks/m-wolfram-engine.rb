36efc193eeab5fedd90a83fd6bac6c24c1dcbfca
Miccal Matthews <miccal.matthews@gmail.com>
2025-01-13 13:33:47 +0800

    Update m-wolfram-engine.rb

--- Casks/m-wolfram-engine.rb
+++ Casks/m-wolfram-engine.rb
@@ -11,20 +11,13 @@ cask "m-wolfram-engine" do
   app "Wolfram Engine.app"
   pkg "WolframScript.pkg"
 
-  postflight do
-    system_command "/bin/rm",
-                   args:         ["-r", "#{Dir.home}/Library/Jupyter/kernels/wolframlanguage14.1"],
-                   sudo:         true,
-                   must_succeed: false
-  end
-
   caveats <<~EOS
     The Wolfram Language can be added as a JupyterLab kernel by running the command
       git clone https://github.com/WolframResearch/WolframLanguageForJupyter.git
     in a suitable directory, then running the command
       cd WolframLanguageForJupyter ; wolframscript -code configure-jupyter.wls add
-    The postflight block removes the JupyterLab kernel for the previous version of the Wolfram Language.
-    To check the current list of JupyterLab kernels, run the command
+    Remove the JupyterLab kernel for the previous version of #{token} using sudo rm -r.
+    Check the current list of JupyterLab kernels by run the command
       jupyter kernelspec list
   EOS
 end
