366513f428261b713fabbe93a3b7e1bc48c95104
Miccal Matthews <miccal.matthews@gmail.com>
2025-01-23 09:31:39 +0800

    Update m-pearcleaner.rb

--- Casks/m-pearcleaner.rb
+++ Casks/m-pearcleaner.rb
@@ -2,7 +2,7 @@ cask "m-pearcleaner" do
   version "4.0.6"
   sha256 :no_check
 
-  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner-Installer.dmg"
+  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip"
 
   livecheck do
     url :url
