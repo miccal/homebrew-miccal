26a7cad29ef54de91f01ffa43bb030c19b32eddf
Miccal Matthews <miccal.matthews@gmail.com>
2025-01-30 09:16:19 +0800

    Create m-leader-key.rb

---
+++ Casks/m-leader-key.rb
@@ -0,0 +1,19 @@
+cask "m-leader-key" do
+  version "1.6.0"
+  sha256 :no_check
+
+  url "https://github.com/mikker/LeaderKey.app/releases/download/v#{version}/Leader.Key.#{version}.zip"
+
+  livecheck do
+    url :url
+    strategy :github_releases do |json|
+      json.map do |release|
+        next if release["draft"]
+
+        release["tag_name"]
+      end
+    end
+  end
+
+  app "Leader Key.app"
+end
