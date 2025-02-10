c5605caf440f7e65722197103cff1d2d285681db
Miccal Matthews <miccal.matthews@gmail.com>
2025-02-11 05:06:32 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -1,7 +1,7 @@
 cask "m-mpv" do
   arch arm: "15-arm", intel: "13-intel"
 
-  version "2025.02.09"
+  version "2025.02.10"
   sha256 :no_check
 
   url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-macos-#{arch}.zip"
