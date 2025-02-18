6a77544168e4374ac6f109b6931d2cf79af56eb0
Miccal Matthews <miccal.matthews@gmail.com>
2025-02-19 05:02:53 +0800

    Update m-dropbox.rb

--- Casks/m-dropbox.rb
+++ Casks/m-dropbox.rb
@@ -1,7 +1,7 @@
 cask "m-dropbox" do
   arch arm: "&arch=arm64"
 
-  version "218.3.4298"
+  version "219.3.4324"
   sha256 :no_check
 
   url "https://www.dropbox.com/download?build=#{version}&plat=mac&rtoken=&type=full#{arch}"
