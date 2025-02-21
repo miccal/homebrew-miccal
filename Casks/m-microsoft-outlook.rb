8bb20bbef0d40dcd3fef3ed7a647fae25b6e955b
Miccal Matthews <miccal.matthews@gmail.com>
2025-02-21 09:06:33 +0800

    Update m-microsoft-outlook.rb

--- Casks/m-microsoft-outlook.rb
+++ Casks/m-microsoft-outlook.rb
@@ -1,5 +1,5 @@
 cask "m-microsoft-outlook" do
-  version "16.94.25020927"
+  version "16.94.25021618"
   sha256 :no_check
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Outlook_#{version}_Installer.pkg"
