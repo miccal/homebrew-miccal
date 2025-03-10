9b7ba3f7d2a3b093daad89bec9658227517f6fb9
Miccal Matthews <miccal.matthews@gmail.com>
2025-03-10 22:11:13 +0800

    Update m-mactex.rb

--- Casks/m-mactex.rb
+++ Casks/m-mactex.rb
@@ -2,10 +2,10 @@ cask "m-mactex" do
   version "2025,0308,10.04.0"
   sha256 :no_check
 
-  url "https://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/mactex-#{version.csv.first}#{version.csv.second}.pkg"
+  url "https://au.mirrors.cicku.me/ctan/systems/mac/mactex/mactex-#{version.csv.first}#{version.csv.second}.pkg"
 
   livecheck do
-    url "https://ctan.org/texarchive/systems/mac/mactex/"
+    url "https://au.mirrors.cicku.me/ctan/systems/mac/mactex/"
     strategy :page_match do |page|
       year = page.scan(/mactex[._-]v?(\d{4})\d{4}\.pkg/i)
                  .flatten
