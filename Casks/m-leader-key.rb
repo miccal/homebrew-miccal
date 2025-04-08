9630e2a2fa6ff3e895de11195833452d32cf3487
Miccal Matthews <miccal.matthews@gmail.com>
2025-04-09 05:33:29 +0800

    Update m-leader-key.rb

--- Casks/m-leader-key.rb
+++ Casks/m-leader-key.rb
@@ -1,5 +1,5 @@
 cask "m-leader-key" do
-  version "1.12.0"
+  version "1.13.0"
   sha256 :no_check
 
   url "https://github.com/mikker/LeaderKey.app/releases/download/v#{version}/Leader.Key.app.zip"
