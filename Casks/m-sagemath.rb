c67e1c3d5dfae88cf70ea5ef6cc252442b143e66
Miccal Matthews <miccal.matthews@gmail.com>
2025-04-09 05:34:44 +0800

    Update m-sagemath.rb

--- Casks/m-sagemath.rb
+++ Casks/m-sagemath.rb
@@ -1,7 +1,7 @@
 cask "m-sagemath" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "10.5,2.5.0"
+  version "10.6,2.6.0"
   sha256 :no_check
 
   url "https://github.com/3-manifolds/Sage_macOS/releases/download/v#{version.csv.second}/SageMath-#{version.csv.first}_#{arch}.dmg"
