e742d4b4720b57f0659f58d40f5be2315de24086
Miccal Matthews <miccal.matthews@gmail.com>
2025-04-15 16:51:44 +0800

    Update m-proxyman.rb

--- Casks/m-proxyman.rb
+++ Casks/m-proxyman.rb
@@ -1,5 +1,5 @@
 cask "m-proxyman" do
-  version "5.17.0,51700"
+  version "5.18.0,51800"
   sha256 :no_check
 
   url "https://download.proxyman.com/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
