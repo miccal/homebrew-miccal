7f916ae1e353d5d81d8bac635dda71e0ff116508
Miccal Matthews <miccal.matthews@gmail.com>
2025-04-22 17:24:10 +0800

    Update m-ipe.rb

--- Casks/m-ipe.rb
+++ Casks/m-ipe.rb
@@ -1,10 +1,8 @@
 cask "m-ipe" do
-  arch arm: "arm", intel: "intel"
-
   version "7.2.29"
   sha256 :no_check
 
-  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipe-#{version}-mac-#{arch}.dmg"
+  url "https://github.com/otfried/ipe/releases/download/v#{version}/ipe-#{version}-mac-arm.dmg"
 
   livecheck do
     cask "ipe"
