2daa21d3954c7e499efb809521217ea652d8d908
Miccal Matthews <miccal.matthews@gmail.com>
2025-04-22 17:25:19 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -1,20 +1,17 @@
 cask "m-libreoffice" do
-  arch arm: "aarch64", intel: "x86-64"
-  folder = on_arch_conditional arm: "aarch64", intel: "x86_64"
-
   version "25.2.3.1"
   sha256 :no_check
 
   # Stable:
   #url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
   # RC:
-  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOffice_#{version}_MacOS_#{arch}.dmg"
+  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/aarch64/LibreOffice_#{version}_MacOS_aarch64.dmg"
   # Dev:
-  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_#{arch}.dmg"
+  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/#{folder}/LibreOfficeDev_#{version}_MacOS_aarch64.dmg"
 
   livecheck do
     url "https://download.documentfoundation.org/libreoffice/testing/"
-    regex(/LibreOffice(?:Dev)?[._-]v?(.+?)[._-]MacOS[._-]#{arch}\.dmg/i)
+    regex(/LibreOffice(?:Dev)?[._-]v?(.+?)[._-]MacOS[._-]aarch64\.dmg/i)
     strategy :page_match do |page, regex|
       versions = page.scan(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
                      .flatten
@@ -24,7 +21,7 @@ cask "m-libreoffice" do
       next if versions.blank?
 
       page = Homebrew::Livecheck::Strategy.page_content(
-        URI.join(@url, "#{versions.last}/mac/#{folder}/").to_s,
+        URI.join(@url, "#{versions.last}/mac/aarch64/").to_s,
       )
       next if page[:content].blank?
 
