419d01bb0a6f475fb4624d248f3b6a85a987ae33
Miccal Matthews <miccal.matthews@gmail.com>
2025-05-21 20:32:41 +0800

    Update m-processspy.rb

--- Casks/m-processspy.rb
+++ Casks/m-processspy.rb
@@ -2,7 +2,7 @@ cask "m-processspy" do
   version "1.7.1"
   sha256 :no_check
 
-  url "https://process-spy.app/archive/ProcessSpy_#{version}.dmg"
+  url "https://process-spy.app/archive/ProcessSpy_#{version}.zip"
 
   livecheck do
     cask "processspy"
