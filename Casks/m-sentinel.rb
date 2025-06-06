b64fddd4b911874315b36205338ecd177c169dd7
Miccal Matthews <miccal.matthews@gmail.com>
2025-06-07 02:56:52 +0800

    Create m-sentinel.rb

---
+++ Casks/m-sentinel.rb
@@ -0,0 +1,8 @@
+cask "m-sentinel" do
+  version "3.0.0"
+  sha256 :no_check
+
+  url "https://github.com/alienator88/Sentinel/releases/download/#{version}/Sentinel.zip"
+
+  app "Pearcleaner.app"
+end
