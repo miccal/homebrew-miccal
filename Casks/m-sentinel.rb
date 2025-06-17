725a821df247ac88c3880087ff21c14d502590c2
Miccal Matthews <miccal.matthews@gmail.com>
2025-06-17 08:20:49 +0800

    Update m-sentinel.rb

--- Casks/m-sentinel.rb
+++ Casks/m-sentinel.rb
@@ -4,5 +4,9 @@ cask "m-sentinel" do
 
   url "https://github.com/alienator88/Sentinel/releases/download/#{version}/Sentinel.zip"
 
+  livecheck do
+    cask "alienator88-sentinel"
+  end
+
   app "Sentinel.app"
 end
