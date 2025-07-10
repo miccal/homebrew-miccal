b2dd492e0f2a8071673f90e2002073e563167a7f
Miccal Matthews <miccal.matthews@gmail.com>
2025-07-10 13:54:53 +0800

    Update m-slack.rb

--- Casks/m-slack.rb
+++ Casks/m-slack.rb
@@ -1,5 +1,5 @@
 cask "m-slack" do
-  version "4.45.52"
+  version "4.45.60"
   sha256 :no_check
 
   url "https://downloads.slack-edge.com/desktop-releases/mac/arm64/#{version}/Slack-#{version}-macOS.dmg"
