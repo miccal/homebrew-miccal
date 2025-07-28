5e97e7d0926371e09c682fa8cda26a67138e9388
Miccal Matthews <miccal.matthews@gmail.com>
2025-07-29 05:21:52 +0800

    Update m-microsoft-teams.rb

--- Casks/m-microsoft-teams.rb
+++ Casks/m-microsoft-teams.rb
@@ -1,5 +1,5 @@
 cask "m-microsoft-teams" do
-  version "25163.3001.3726.6503"
+  version "25185.205.3812.7741"
   sha256 :no_check
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/MicrosoftTeams.pkg"
