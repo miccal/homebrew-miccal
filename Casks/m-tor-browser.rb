f830b12a33c0ba8ba25dc8cec6ff9a5436820d7f
Miccal Matthews <miccal.matthews@gmail.com>
2025-08-20 12:08:44 +0800

    Update m-tor-browser.rb

--- Casks/m-tor-browser.rb
+++ Casks/m-tor-browser.rb
@@ -1,5 +1,5 @@
 cask "m-tor-browser" do
-  version "15.0a1"
+  version "14.5.6"
   sha256 :no_check
 
   url "https://dist.torproject.org/torbrowser/#{version}/tor-browser-macos-#{version}.dmg"
@@ -16,7 +16,7 @@ cask "m-tor-browser" do
   end
 
   # Stable:
-  #app "Tor Browser.app"
+  app "Tor Browser.app"
   # Alpha:
-  app "Tor Browser Alpha.app", target: "Tor Browser.app"
+  #app "Tor Browser Alpha.app", target: "Tor Browser.app"
 end
