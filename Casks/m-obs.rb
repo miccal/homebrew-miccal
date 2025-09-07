569f959c23f32900819b2b28b3adc5aba26f38b5
Miccal Matthews <miccal.matthews@gmail.com>
2025-09-07 18:54:39 +0800

    Update m-obs.rb

--- Casks/m-obs.rb
+++ Casks/m-obs.rb
@@ -1,5 +1,5 @@
 cask "m-obs" do
-  version "32.0.0-beta1"
+  version "32.0.0-beta2"
   sha256 :no_check
 
   url "https://github.com/obsproject/obs-studio/releases/download/#{version}/OBS-Studio-#{version}-macOS-Apple.dmg"
