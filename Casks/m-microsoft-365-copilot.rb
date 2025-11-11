98171f116cd480fc29ba4f2bbf5940db5175f57d
Miccal Matthews <miccal.matthews@gmail.com>
2025-11-12 05:06:12 +0800

    Update m-microsoft-365-copilot.rb

--- Casks/m-microsoft-365-copilot.rb
+++ Casks/m-microsoft-365-copilot.rb
@@ -1,5 +1,5 @@
 cask "m-microsoft-365-copilot" do
-  version "1.2511.0301"
+  version "1.2511.0501"
 
   url "https://m365copilotformac.blob.core.windows.net/releases/Microsoft_365_Copilot_universal_#{version}_Installer.pkg"
 
