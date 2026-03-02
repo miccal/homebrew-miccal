d941771ac42b47049a721033e3a13988268d9f26
Miccal Matthews <miccal.matthews@gmail.com>
2026-03-02 13:32:34 +0800

    Update m-mactex.rb

--- Casks/m-mactex.rb
+++ Casks/m-mactex.rb
@@ -1,7 +1,8 @@
 cask "m-mactex" do
   version "2026,0301,10.06.0"
 
-  url "https://au.mirrors.cicku.me/ctan/systems/mac/mactex/mactex-#{version.csv.first}#{version.csv.second}.pkg"
+  #url "https://au.mirrors.cicku.me/ctan/systems/mac/mactex/mactex-#{version.csv.first}#{version.csv.second}.pkg"
+  url "https://mirrors.ctan.org/systems/mac/mactex/mactex-{version.csv.first}#{version.csv.second}.pkg"
 
   livecheck do
     #url "https://au.mirrors.cicku.me/ctan/systems/mac/mactex/"
