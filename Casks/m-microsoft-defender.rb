3bddf8d5fd2e7177ebe076bf7555648d311454fb
Miccal Matthews <miccal.matthews@gmail.com>
2026-03-11 07:53:51 +0800

    Update m-microsoft-defender.rb

--- Casks/m-microsoft-defender.rb
+++ Casks/m-microsoft-defender.rb
@@ -1,5 +1,5 @@
 cask "m-microsoft-defender" do
-  version "101.25122.0007"
+  version "101.26012.0012"
 
   url "https://officecdnmac.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Defender_#{version}_Individuals_Installer.pkg"
 
