25e09e90e5db15973e5c395eafd1297f492f0f1e
Miccal Matthews <miccal.matthews@gmail.com>
2026-03-17 12:19:45 +0800

    Update m-microsoft-teams.rb

--- Casks/m-microsoft-teams.rb
+++ Casks/m-microsoft-teams.rb
@@ -2,9 +2,11 @@ cask "m-microsoft-teams" do
   version "25290.302.4044.3989"
 
   url "https://statics.teams.cdn.office.net/production-osx/#{version}/MicrosoftTeams.pkg"
-
   livecheck do
-    cask "microsoft-teams"
+    url "https://config.teams.microsoft.com/config/v1/MicrosoftTeams/#{version}?environment=prod&audienceGroup=general&teamsRing=general&agent=TeamsBuilds"
+    strategy :json do |json|
+      json.dig("BuildSettings", "WebView2Canary", "macOS", "latestVersion")
+    end
   end
 
   pkg "MicrosoftTeams.pkg",
