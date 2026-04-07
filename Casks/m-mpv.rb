e1a1d8f1af6372a8c52f511d9a0fff4fdb7d6ed7
Miccal Matthews <miccal.matthews@gmail.com>
2026-04-08 07:47:27 +0800

    Update m-mpv.rb

--- Casks/m-mpv.rb
+++ Casks/m-mpv.rb
@@ -1,5 +1,5 @@
 cask "m-mpv" do
-  version "0.41.0-dev-gef7bced02-23505728086"
+  version "0.41.0-dev-ge2180e581-24040995550"
 
   url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-v#{version}-macos-26-arm.zip"
 
