4a8197a89c4fbdae3c718de9d95e8d2d55daac2b
Miccal Matthews <miccal.matthews@gmail.com>
2026-05-23 18:46:23 +0800

    Update m-tuna.rb

--- Casks/m-tuna.rb
+++ Casks/m-tuna.rb
@@ -4,16 +4,7 @@ cask "m-tuna" do
   url "https://tunaformac.com/download/releases/#{version.csv.second}"
 
   livecheck do
-    #url "https://tunaformac.com/appcast.xml"
-    #strategy :sparkle, &:short_version
-    url "https://tunaformac.com/download/latest"
-    regex(/tuna[._-]v?(\d+(?:\.\d+)+)[._-]v?(\d+)\.zip/i)
-    strategy :header_match do |headers, regex|
-      match = headers["location"]&.match(regex)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
-    end
+    cask "tuna"
   end
 
   app "Tuna.app"
