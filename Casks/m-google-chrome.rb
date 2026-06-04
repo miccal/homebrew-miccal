b391aa9582098c149e1b31fe911e5d4fbb4e029e
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-04 17:46:47 +0800

    Update m-google-chrome.rb

--- Casks/m-google-chrome.rb
+++ Casks/m-google-chrome.rb
@@ -3,12 +3,5 @@ cask "m-google-chrome" do
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 
-  #livecheck do
-  #  url "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Mac"
-  #  strategy :json do |json|
-  #    json.map { |item| item["version"] }
-  #  end
-  #end
-
   app "Google Chrome.app"
 end
