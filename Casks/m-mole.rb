f1029ef1f3c1b4c26478ba3a47d9d3098afd1c77
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-17 07:58:08 +0800

    Update m-mole.rb

--- Casks/m-mole.rb
+++ Casks/m-mole.rb
@@ -1,10 +1,10 @@
 cask "m-mole" do
-  version "1.7.1"
+  version "1.8.0"
 
   # Stable:
-  url "https://mole.fit/Mole.dmg"
+  #url "https://mole.fit/Mole.dmg"
   # Preview:
-  #url "https://mole.fit/Mole-preview.dmg"
+  url "https://mole.fit/Mole-preview.dmg"
 
   livecheck do
     cask "mole-app"
