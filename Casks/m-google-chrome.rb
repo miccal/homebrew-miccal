a6cb1757088d8c13552813bdb43601f2a4c6a9cf
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-24 04:27:28 +0800

    Update m-google-chrome.rb

--- Casks/m-google-chrome.rb
+++ Casks/m-google-chrome.rb
@@ -4,4 +4,12 @@ cask "m-google-chrome" do
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 
   app "Google Chrome.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Google Chrome.app"],
+                   sudo:         false,
+                   must_succeed: false,
+                   print_stderr: false
+  end
 end
