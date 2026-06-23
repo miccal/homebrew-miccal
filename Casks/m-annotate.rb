3ba92073643e9ff1b300b8f947e883cb48648d7a
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-23 15:02:28 +0800

    Update m-annotate.rb

--- Casks/m-annotate.rb
+++ Casks/m-annotate.rb
@@ -4,4 +4,12 @@ cask "m-annotate" do
   url "https://github.com/epilande/Annotate/releases/download/v#{version}/Annotate-#{version}.dmg"
 
   app "Annotate.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Annotate.app"],
+                   sudo:         false,
+                   must_succeed: false,
+                   print_stderr: false
+  end
 end
