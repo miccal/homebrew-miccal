110f1a251bec42e5ca17a7984ecb28058e39e8f9
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-24 10:37:47 +0800

    Update m-tikz-editor.rb

--- Casks/m-tikz-editor.rb
+++ Casks/m-tikz-editor.rb
@@ -1,7 +1,7 @@
 cask "m-tikz-editor" do
   version "0.4.0"
 
-  url "https://github.com/DominikPeters/tikz-editor/releases/download/app-v#{version}/TikZ.Editor_#{version}_#{arch}.dmg"
+  url "https://github.com/DominikPeters/tikz-editor/releases/download/app-v#{version}/TikZ.Editor_#{version}_aarch64.dmg"
 
   app "TikZ Editor.app"
 end
