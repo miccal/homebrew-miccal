dfa08dd780d4cde366fc6dcf0ef94825be3a6d47
Miccal Matthews <miccal.matthews@gmail.com>
2026-06-24 10:38:58 +0800

    Update m-tikz-editor.rb

--- Casks/m-tikz-editor.rb
+++ Casks/m-tikz-editor.rb
@@ -4,4 +4,12 @@ cask "m-tikz-editor" do
   url "https://github.com/DominikPeters/tikz-editor/releases/download/app-v#{version}/TikZ.Editor_#{version}_aarch64.dmg"
 
   app "TikZ Editor.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/TikZ Editor.app"],
+                   sudo:         false,
+                   must_succeed: false,
+                   print_stderr: false
+  end
 end
