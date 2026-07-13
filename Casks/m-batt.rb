0c862c6b1c6e5d5e7f39cdf5a6fd5adde2e725cc
Miccal Matthews <miccal.matthews@gmail.com>
2026-07-14 05:27:39 +0800

    Update m-batt.rb

--- Casks/m-batt.rb
+++ Casks/m-batt.rb
@@ -1,7 +1,15 @@
 cask "m-batt" do
-  version "0.7.4"
+  version "0.7.5"
 
   url "https://github.com/charlie0129/batt/releases/download/v#{version}/batt-v#{version}.dmg"
 
   app "batt.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/batt.app"],
+                   sudo:         false,
+                   must_succeed: false,
+                   print_stderr: false
+  end
 end
