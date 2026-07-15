53646798d42c3b63c8e34a45e6867767698fdbfe
Miccal Matthews <miccal.matthews@gmail.com>
2026-07-16 04:49:25 +0800

    Update m-libreoffice.rb

--- Casks/m-libreoffice.rb
+++ Casks/m-libreoffice.rb
@@ -1,12 +1,12 @@
 cask "m-libreoffice" do
-  version "26.8.0.0.beta1"
+  version "26.8.0.1"
 
   # Stable:
   #url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/aarch64/LibreOffice_#{version}_MacOS_aarch64.dmg"
   # RC:
-  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/aarch64/LibreOffice_#{version}_MacOS_aarch64.dmg"
+  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/aarch64/LibreOffice_#{version}_MacOS_aarch64.dmg"
   # Dev:
-  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/aarch64/LibreOfficeDev_#{version}_MacOS_aarch64.dmg"
+  #url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/aarch64/LibreOfficeDev_#{version}_MacOS_aarch64.dmg"
 
   livecheck do
     url "https://download.documentfoundation.org/libreoffice/testing/"
@@ -29,16 +29,16 @@ cask "m-libreoffice" do
   end
 
   # Stable, RC:
-  #app "LibreOffice.app"
+  app "LibreOffice.app"
   # Dev:
-  app "LibreOfficeDev.app", target: "LibreOffice.app"
+  #app "LibreOfficeDev.app", target: "LibreOffice.app"
 
   postflight do
     system_command "/usr/bin/xattr",
                    # Stable, RC:
-                   #args:         ["-d", "com.apple.quarantine", "#{staged_path}/LibreOffice.app"],
+                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/LibreOffice.app"],
                    # Dev:
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/LibreOfficeDev.app"],
+                   #args:         ["-d", "com.apple.quarantine", "#{staged_path}/LibreOfficeDev.app"],
                    sudo:         false,
                    must_succeed: false,
                    print_stderr: false
