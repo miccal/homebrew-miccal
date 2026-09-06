aba90dbf2c0a486f9037aa3cc39b14038422ad24
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:06:31 +0800

    Update m-batt.rb

--- Casks/m-batt.rb
+++ Casks/m-batt.rb
@@ -5,11 +5,11 @@ cask "m-batt" do
 
   app "batt.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/batt.app"],
-                   sudo:         false,
-                   must_succeed: false,
-                   print_stderr: false
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:         ["-d", "com.apple.quarantine", "/Applications/batt.app"],
+        sudo:         false,
+        must_succeed: false,
+        print_stderr: false
   end
 end
