6dd6c3b5d964f7c058ac0133e45d9fb9b644798f
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:16:12 +0800

    Update m-tuna.rb

--- Casks/m-tuna.rb
+++ Casks/m-tuna.rb
@@ -9,11 +9,11 @@ cask "m-tuna" do
 
   app "Tuna.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Tuna.app"],
-                   sudo:         false,
-                   must_succeed: false,
-                   print_stderr: false
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:         ["-d", "com.apple.quarantine", "/Applications/Tuna.app"],
+        sudo:         false,
+        must_succeed: false,
+        print_stderr: false
   end
 end
