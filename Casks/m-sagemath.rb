5e183f88ee12472802221077923be2ae5f3d0bea
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:18:56 +0800

    Update m-sagemath.rb

--- Casks/m-sagemath.rb
+++ Casks/m-sagemath.rb
@@ -18,7 +18,7 @@ cask "m-sagemath" do
 
   postflight_steps do
     run "/usr/bin/xattr",
-        args:         ["-d", "com.apple.quarantine", "/Applications/SageMath-#{version.csv.first.dots_to_hyphens}.app"],
+        args:         ["-d", "com.apple.quarantine", "/Applications/SageMath-10-9.app"],
         sudo:         false,
         must_succeed: false,
         print_stderr: false
