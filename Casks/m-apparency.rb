0e2d8f486a860a0d0fb6d6921afa70f5fcd14942
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 14:22:41 +0800

    Update m-apparency.rb

--- Casks/m-apparency.rb
+++ Casks/m-apparency.rb
@@ -11,7 +11,7 @@ cask "m-apparency" do
 
   postflight_steps do
     run "/usr/bin/xattr",
-        args:         ["-d", "com.apple.quarantine", "/Applications/Apparency.app"],
+        args:         ["-d", "com.apple.quarantine", "{{staged_path}}/Apparency.app"],
         sudo:         false,
         must_succeed: false,
         print_stderr: false
