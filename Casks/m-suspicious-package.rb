7679fd7693d887f757b60db4ab9d541615f77575
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 14:26:37 +0800

    Update m-suspicious-package.rb

--- Casks/m-suspicious-package.rb
+++ Casks/m-suspicious-package.rb
@@ -11,7 +11,7 @@ cask "m-suspicious-package" do
 
   postflight_steps do
     run "/usr/bin/xattr",
-        args:         ["-d", "com.apple.quarantine", "/Applications/Suspicious Package.app"],
+        args:         ["-d", "com.apple.quarantine", "{{staged_path}}/Suspicious Package.app"],
         sudo:         true,
         must_succeed: false,
         print_stderr: false
