611bcd470b930ec81806f803c01ad84912dfd94f
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:12:46 +0800

    Update m-proxyman.rb

--- Casks/m-proxyman.rb
+++ Casks/m-proxyman.rb
@@ -9,11 +9,11 @@ cask "m-proxyman" do
 
   app "Proxyman.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Proxyman.app"],
-                   sudo:         true,
-                   must_succeed: false,
-                   print_stderr: false
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:         ["-d", "com.apple.quarantine", "/Applications/Proxyman.app"],
+        sudo:         true,
+        must_succeed: false,
+        print_stderr: false
   end
 end
