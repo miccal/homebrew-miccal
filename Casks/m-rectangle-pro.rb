0cfcd1714553f54189d5ebaaacf796eae8af8dc2
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:13:16 +0800

    Update m-rectangle-pro.rb

--- Casks/m-rectangle-pro.rb
+++ Casks/m-rectangle-pro.rb
@@ -9,11 +9,11 @@ cask "m-rectangle-pro" do
 
   app "Rectangle Pro.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Rectangle Pro.app"],
-                   sudo:         false,
-                   must_succeed: false,
-                   print_stderr: false
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:         ["-d", "com.apple.quarantine", "/Applications/Rectangle Pro.app"],
+        sudo:         false,
+        must_succeed: false,
+        print_stderr: false
   end
 end
