8ca6f9253a057696a051ad5b430e767390d37da4
Miccal Matthews <miccal.matthews@gmail.com>
2026-09-06 09:07:36 +0800

    Update m-google-chrome.rb

--- Casks/m-google-chrome.rb
+++ Casks/m-google-chrome.rb
@@ -5,11 +5,11 @@ cask "m-google-chrome" do
 
   app "Google Chrome.app"
 
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "#{staged_path}/Google Chrome.app"],
-                   sudo:         true,
-                   must_succeed: false,
-                   print_stderr: false
+  postflight_steps do
+    run "/usr/bin/xattr",
+        args:         ["-d", "com.apple.quarantine", "/Applications/Google Chrome.app"],
+        sudo:         true,
+        must_succeed: false,
+        print_stderr: false
   end
 end
